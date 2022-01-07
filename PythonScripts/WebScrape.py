d0846152148f38036e1cac9f7475dabfa39e1231
j-grady <jgrad8@gmail.com>
2022-01-07 15:35:13 -0600

    Initial Commit

---
+++ PythonScripts/WebScrape.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests as req
+import pandas as pd
+
+url = "https://www.rhinohockeysiouxfalls.com/21-22-crl-winter-league-stats/"
+
+request = req.get(url).text
+
+print(request)
+
+# TDOD: Properly scrap data and store to local DB.
+
+
