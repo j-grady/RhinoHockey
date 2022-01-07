8808d846523afb570833b7388e414da638149532
j-grady <jgrad8@gmail.com>
2022-01-07 17:00:19 -0600

    Database Connection Config

--- PythonScripts/WebScrape.py
+++ PythonScripts/WebScrape.py
@@ -1,11 +1,26 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests as req
 import pandas as pd
+import pyodbc as db
 
 url = "https://www.rhinohockeysiouxfalls.com/21-22-crl-winter-league-stats/"
 
 request = req.get(url).text
+soup = bs(request, "lxml")
 
-print(request)
+# Database Connection Settings
+serverName = "DESKTOP-8FK7M67\SQLEXPRESS"
+databaseName = "RhinoHockey"
+connectionString = "Driver={{SQL Server}};Server={};Database={}".format(serverName, databaseName)
+
+# DB Operations
+cnxn = db.connect(connectionString)
+
+cursor = cnxn.cursor()
+
+cursor.execute('''DROP TABLE IF EXISTS dbo.Test CREATE TABLE dbo.Test (Team VARCHAR(255), Wins INT, Loses INT)''')
+
+cursor.commit()
+cursor.close()
 
 # TDOD: Properly scrap data and store to local DB.
